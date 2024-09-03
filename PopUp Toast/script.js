630b721e8a2329a995c3d8595f3cd8f6f8084416
RasithAbdul <123852033+RasithAbdul@users.noreply.github.com>
2024-09-03 21:35:53 +0530

    Add files via upload

---
+++ PopUp Toast/script.js
@@ -0,0 +1,7 @@
+let popup = document.getElementById('popup');
+function HandleOpen(){
+    open-popup.classList.add('open-popup')
+}
+function HandleClose(){
+    open-popup.classList.remove('open-popup')
+}
